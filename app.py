eef9d7f04b8ce59f70d5f9cf4cef518900ef2c2f
Giselle Chacon <gisecn@gmail.com>
2020-06-09 10:25:40 +0100

    Fix bug in python file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
@@ -9,11 +9,14 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
-@app.route('/get_task')
-def get_task():
-    return render_template('task.html', task=mongo.db.tasks.find())
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', '0.0.0.0'), port = int(os.environ.get('PORT', 5000)), debug=True)
-
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
