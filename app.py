f48fd78bd14d3feab37c43995d0ac0e6c19f0e4f
Giselle Chacon <gisecn@gmail.com>
2020-06-09 12:11:58 +0100

    Testing app in heroku

--- app.py
+++ app.py
@@ -26,4 +26,6 @@ def get_tasks():
 
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', 5000), debug=True)
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', 5000)),
+            debug=True)
