06cb73e8ba08755d488d25fc4aa13bc6b6f1ff03
Giselle Chacon <gisecn@gmail.com>
2020-06-09 10:05:34 +0100

    Fix bug in python file

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-naitp.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
