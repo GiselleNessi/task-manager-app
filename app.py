c39694c20436794ea78e87e6175381aaea374db0
Giselle Chacon <gisecn@gmail.com>
2020-06-09 11:12:23 +0100

    Fix bug

--- app.py
+++ app.py
@@ -24,5 +24,6 @@ def get_tasks():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
