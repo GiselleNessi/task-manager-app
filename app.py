d1f118d1ea517d8e90337162ea397e783a804e66
Giselle Chacon <gisecn@gmail.com>
2020-06-09 09:57:08 +0100

    Fix bug in PORT

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def get_task():
     return render_template('task.html', task=mongo.db.tasks.find())
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), port=os.environ.get('PORT'), debug=True)
+    app.run(host=os.environ.get('IP', '0.0.0.0'), port=os.environ.get('PORT', 5000), debug=True)
 
